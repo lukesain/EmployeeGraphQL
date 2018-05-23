e660248f95c20b45b8908222506df4c42a456466
Luke Spires <Luke.Spires@sainsburys.co.uk>
2018-05-23 08:56:23 +0100

    added missing project for Graphiql

---
+++ EmployeeGraphQL.GraphiQL/GraphiQLMiddlewareExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeGraphQL.GraphiQL
+{
+    public static class GraphiqlMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseGraphiQL(
+            this IApplicationBuilder builder,
+            PathString path,
+            Action<GraphiQLMiddlewareOptions> configure)
+        {
+            var options = new GraphiQLMiddlewareOptions();
+            configure(options);
+
+            return builder.UseGraphiQL(path, options);
+        }
+
+        public static IApplicationBuilder UseGraphiQL(
+            this IApplicationBuilder builder,
+            PathString path,
+            GraphiQLMiddlewareOptions options)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            return builder.Map(path, branch => branch.UseMiddleware<GraphiQLMiddleware>(options));
+        }
+    }
+}
---
+++ EmployeeGraphQL.GraphiQL/GraphiqlMiddlewareOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeGraphQL.GraphiQL
+{
+    public class GraphiQLMiddlewareOptions
+    {
+        public string GraphQLEndpoint { get; set; } = "/graph";
+    }
+}
